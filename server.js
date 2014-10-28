a2a59f526cbfd0d19a0090a190d20ccebde2fa78
yvettecook <yvettecook@gmail.com>
2014-10-28 16:46:59 +0000

    Chat up and working

--- server.js
+++ server.js
@@ -15,7 +15,8 @@ io.on('connection', function(socket){
 
 io.on('connection', function(socket) {
 	socket.on('chat message', function(msg) {
-		console.log('message: ' + msg);
+		io.emit('chat message', msg);
+		console.log(msg);
 	});
 });
 
